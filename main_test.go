14a018c2635b6116149e500016e1d60a9e28f8a3
Nozch <freegia@gmail.com>
2023-07-24 18:59:20 +0900

    Login, Verify test works. Users test needs refactoring for database connection

---
+++ main_test.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLoginHandler(t *testing.T) {
+	reqBody := bytes.NewBuffer(json.RawMessage(`{
+		"username": "user1",
+		"password": "password1"
+	}`))
+
+	req, err := http.NewRequest("POST", "/login", reqBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(LoginHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusCreated)
+	}
+
+	expected := `{"token":`
+	if rr.Body.String()[:len(expected)] != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestVerifyHandler(t *testing.T) {
+	token, err := GenerateToken("user1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("GET", "/verify", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(VerifyHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Hello, user1"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestUsersHandler_GET(t *testing.T) {
+	// Set up a mock database connection
+	mockDB := new(mocks.DB)
+	// Set up the handler
+	handler := UsersHandler{
+		DB: mockDB,
+	}
+
+	// Prepare a test user
+	user := User{
+		ID:           1,
+		Username:     "user1",
+		PasswordHash: "password1_hash",
+	}
+	// Set up the mock database to return the test user
+	mockDB.On("Select", mock.Anything, mock.Anything).Return(user, nil)
+
+	// Create a request
+	req, err := http.NewRequest("GET", "/users/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Record the response
+	rr := httptest.NewRecorder()
+
+	// Call the handler
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check the response body
+	expected := `{"id":1,"username":"user1","password_hash":"password1_hash"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
